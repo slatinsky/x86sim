69ddacd3307682abacd42ac58c283601d0c0b2d0
slatinsky <slatinsky.adam@gmail.com>
2020-12-23 00:06:53 +0100

    added typescript to the project, added ace editor

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,14 @@
-<script>
+
+<svelte:head>
+	<!--	Bootstrap 5 -->
+	<!--	https://getbootstrap.com/-->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+	<!--	fontawesome 5-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+</svelte:head>
+
+<script lang="ts">
 	import Navigation from "./Navigation.svelte"
 	import Photos from "./Photos.svelte";
 	import Registers from "./modules/Registers.svelte";
@@ -50,14 +60,6 @@
 	/*}*/
 </style>
 
-<svelte:head>
-<!--	Bootstrap 5 -->
-<!--	https://getbootstrap.com/-->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-
-<!--	fontawesome 5-->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
-</svelte:head>
 
 <main>
 	<Navigation />
---
+++ src/modules/CodeEditor.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+    import { AceEditor } from "svelte-ace";
+    import "brace/mode/assembly_x86";
+    import "brace/theme/dracula";
+    let text = `// Ukážkový assembler kód
+// Spočítaj čísla v intervale 1-10
+
+mov cx, 10
+xor ax,ax
+
+loop:
+    add ax, cx
+    dec cx
+    jnz loop
+
+end:
+hlt
+`;
+    let editor
+    function init(editor) {
+        window.editor = editor
+
+        // custom line formatting: https://stackoverflow.com/questions/28311086/modify-the-gutter-of-ajax-org-cloud9-editor-ace-editor
+        editor.session.gutterRenderer =  {
+            getWidth: function(session, lastLineNumber, config) {
+                return lastLineNumber.toString().length * config.characterWidth;
+            },
+            getText: function(session, row) {
+
+                // console.log(editor.session.getLine(4))  //get fourth line, will be usefull later
+                return (row+7000).toString().padStart(4, '0');
+            }
+        };
+
+        // hide vertical ruler (after 80 characters in the editor)
+        editor.setShowPrintMargin(false);
+
+        // larger font
+        editor.setOptions({
+            fontSize: 16
+        });
+    }
+
+
+</script>
+
+<b>Editor kódu:</b>
+<!--https://github.com/nateshmbhat/svelte-ace-->
+<AceEditor
+        on:selectionChange={(obj) => console.log(obj.detail)}
+        on:paste={(obj) => console.log(obj.detail)}
+        on:input={(obj) => console.log(obj.detail)}
+        on:focus={() => console.log('focus')}
+        on:documentChange={(obj) => console.log(`document change : ${obj.detail}`)}
+        on:cut={() => console.log('cut')}
+        on:cursorChange={() => console.log('cursor change')}
+        on:copy={() => console.log('copy')}
+        on:init={(editor) => init(editor.detail)}
+        on:commandKey={(obj) => console.log(obj.detail)}
+        on:changeMode={(obj) => console.log(`change mode : ${obj.detail}`)}
+        on:blur={() => console.log('blur')}
+        width='100%'
+        height='300px'
+        lang="assembly_x86"
+        theme="dracula"
+        value={text}
+/>
+
+
