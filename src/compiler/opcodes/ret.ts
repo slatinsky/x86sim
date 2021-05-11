e7cbb23d43ba8a5667ee162c75b823114b7ee2af
slatinsky <slatinsky.adam@gmail.com>
2021-05-12 00:44:01 +0200

    ret instruction now accepts parameter, how many values it should remove from stack

--- src/compiler/opcodes/ret.ts
+++ src/compiler/opcodes/ret.ts
@@ -3,12 +3,12 @@ import {registers, memory} from "@stores/stores";
 // pushes value to stack
 export default  {
     writesTo: ['ip'], // writes to ip, because it is jump
-    run: () => {
+    run: (operand) => {
         // pop ip
         let sp = registers.get('sp')
         let address = (registers.get('ss') << 4) + sp
         let valueFromStack = memory.get(address, 16)
-        registers.set('sp', sp + 2)
+        registers.set('sp', sp + operand.get() * 2)
 
         // jump to ip we got from stack
         registers.set('ip', valueFromStack - 1)
